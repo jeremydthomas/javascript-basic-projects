c7e118bed9d41c13f6da87e80c701094f4679965
Jeremy <tommyhilfiger12@hotmail.com>
2022-03-17 23:15:26 -0500

    added error messages and loading in fetch

--- 22-products/starter/app.js
+++ 22-products/starter/app.js
@@ -1,1 +1,37 @@
-console.log('products starter');
+const url = 'https://course-api.com/javascript-store-products';
+
+const productsDOM = document.querySelector('.products-center');
+
+// returns fetches gets and handles loading of data
+
+const fetchProducts = async () => {
+	productsDOM.innerHTML = `<div class="loading"></div>`;
+	try {
+		const response = await fetch(url);
+		const data = await response.json();
+
+		// if (!response.ok) {
+		// 	throw new Error('there was an error fetching products');
+		// }
+
+		return data;
+	} catch (error) {
+		productsDOM.innerHTML = `<p class="error">there was an error</p>`;
+	}
+};
+
+// displays/renders data 
+
+const displayProducts = (list) => {
+	console.log(list);
+};
+
+// fetch and get and pass data to rendered data
+
+const start = async () => {
+const data = await fetchProducts();
+displayProducts(data);
+};
+
+fetchProducts();
+start();
