1f784112e11312c3b8a866b6c46a9b4a44dd768c
Jeremy <tommyhilfiger12@hotmail.com>
2022-03-20 01:27:39 -0500

    finishe project 23

--- 23-random-user/setup/app.js
+++ 23-random-user/setup/app.js
@@ -1,48 +1,18 @@
 import get from './utils/getElement.js';
+import getUser from './utils/fetchUser.js';
+import displayUser from './utils/displayUser.js';
 
-const URL = 'https://randomuser.me/api/';
 
-const img = get('.user-img');
-const title = get('.user-title');
-const value = get('.user-value');
 const btn = get('.btn');
-const btns = [...document.querySelectorAll('.icon')];
 
-const getUser = async () => {
-	const response = await fetch(URL);
-	const data = await response.json();
-	// destructure
-	const person = data.results[0];
-	const { phone, email } = person;
-	const { large: image } = person.picture;
-	const { password } = person.login;
-	const { first, last } = person.name;
-	const {
-		dob: { age },
-	} = person;
-	const {
-		street: { number, name },
-	} = person.location;
-
-	return {
-		image,
-		phone,
-		email,
-
-		password,
-		first,
-		last,
-		age,
-		street: `${number}${name}`,
-		name: `${first}${last}`,
-	};
-};
 
 const showUser = async () => {
 	const person = await getUser();
-
+	displayUser(person);
 	// display user
 };
 
 window.addEventListener('DOMContentLoaded', showUser);
 btn.addEventListener('click', showUser);
+
+
