ed095561734eb324bb32689d5076e317e23a9e37
Jeremy <tommyhilfiger12@hotmail.com>
2022-03-22 14:23:44 -0500

    loading display added

--- 24-cocktails/setup/src/displayDrinks.js
+++ 24-cocktails/setup/src/displayDrinks.js
@@ -1,24 +1,24 @@
 import get from './getElement.js';
+import { hideLoading } from './toggleLoading.js';
 
 const displayDrinks = ({ drinks }) => {
 	const section = get('.section-center');
 	const title = get('.title');
 	if (!drinks) {
 		// hide loading
+		hideLoading();
 		title.textContent = 'Sorry, no drinks are available';
 		section.innerHTML = null;
 		return;
 	} else {
 		const newDrinks = drinks
 			.map((drink) => {
-
 				const {
 					strDrink: name,
 					strDrinkThumb: image,
 					idDrink: id,
 					strCategory: category,
 				} = drink;
-		
 
 				return `<a href="drink.html">
 					<article class="cocktail" data-id="${id}">
@@ -29,6 +29,7 @@ const displayDrinks = ({ drinks }) => {
 			})
 			.join('');
 		// hide loading
+		hideLoading();
 		title.textContent = '';
 		section.innerHTML = newDrinks;
 		return section;
