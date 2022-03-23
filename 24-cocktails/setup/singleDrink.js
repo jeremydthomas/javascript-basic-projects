978852f3681f7cbec579328b255bb1378d446b13
Jeremy <tommyhilfiger12@hotmail.com>
2022-03-23 16:23:14 -0500

    singledrink

--- 24-cocktails/setup/singleDrink.js
+++ 24-cocktails/setup/singleDrink.js
@@ -1,1 +1,12 @@
-const URL = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11006';
+import fetchDrinks from './src/fetchDrinks.js';
+import displayDrink from './src/displaySingleDrink.js';
+
+const URL = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
+const presentDrink = async () => {
+	const id = localStorage.getItem('drink');
+	const drink = await fetchDrinks(`${URL}${id}`);
+	displayDrink(drink);
+};
+
+window.addEventListener('DOMContentLoaded', presentDrink);
