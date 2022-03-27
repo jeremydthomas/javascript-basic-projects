8def99bff758c59ac01bbca9d39928beff88b8a9
Jeremy <tommyhilfiger12@hotmail.com>
2022-03-27 17:24:35 -0500

    finished up sub menus

--- 26-stripe-submenus/setup/app.js
+++ 26-stripe-submenus/setup/app.js
@@ -43,7 +43,43 @@ linkBtns.forEach((btn) => {
 	btn.addEventListener('mouseover', function (e) {
 		const text = e.currentTarget.textContent;
 		const tempBtn = e.currentTarget.getBoundingClientRect();
-  console.log(tempBtn);
-		submenu.classList.add('show');
+		const center = (tempBtn.left + tempBtn.right) / 2;
+		const bottom = tempBtn.bottom - 3;
+
+		const tempPage = sublinks.find(({ page }) => page === text);
+		if (tempPage) {
+			const { page, links } = tempPage;
+			submenu.classList.add('show');
+			submenu.style.left = `${center}px`;
+			submenu.style.top = `${bottom}px`;
+
+			let columns = 'col-2';
+			if (links.length === 3) {
+				columns = 'col-3';
+			}
+			if (links.length > 3) {
+				columns = 'col-4';
+			}
+
+			submenu.innerHTML = `<section><h4>${page}</h4><div class='submenu-center ${columns}'>
+			${links
+				.map((link) => {
+					return `	<a href='${link.url}'>
+    <i class="${link.icon}"></i>
+    ${link.label}
+    </a>`;
+				})
+				.join('')}
+</div></section>`;
+		}
 	});
+});
+
+hero.addEventListener('mouseover', function (e) {
+	submenu.classList.remove('show');
+});
+nav.addEventListener('mouseover', function (e) {
+	if (!e.target.classList.contains('link-btn')) {
+		submenu.classList.remove('show');
+	}
 });
